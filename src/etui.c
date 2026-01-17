8e1052eef8cb7f986e334e8f8a8a44ae0fa5b000
ellos0 <mcgibbonellott@gmail.com>
2026-01-17 08:19:21 -0800

    removed example programme, moved to eg/hello.c

--- src/etui.c
+++ src/etui.c
@@ -79,17 +79,7 @@ void setup() {
 }
 
 int main() {
-  WindowStyle default_win_style = DEFAULT_WIN_STYLE
-  setup();
-
-  Window w = {2,2,20,8};
-  box_window(&w, &default_win_style);
-  w_move_cursor(&w,1,0);
-  puts("test 1 2 3");
-
-  w_place_wrap_text(&w,4,1, "Hello, world! testing 1 2 3 4 5 6 7 8 9 10");
-  
-  getc(stdin);
+  puts("This is not the executable you are looking for. Refer to doc/INSTALL for further instructions.");
   
   return 0;
 }
